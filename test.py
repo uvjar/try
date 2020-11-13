eaa115515f546a9d82fb4ed555e34c987385d90b
caop <820672172@qq.com>
2020-11-13 16:50:48 +0000

    add parser

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import tflite_runtime.interpreter as tflite
 from PIL import Image
 import numpy as np
 import string
+import argparse
+import os
 
 characters = ' '+ string.digits + string.ascii_lowercase+string.punctuation
 
@@ -19,23 +21,48 @@ def my_ctc_decode(y_pred):
 
 
 def main():
-	interpreter = tflite.Interpreter(model_path="converted_model.tflite")
+	parser = argparse.ArgumentParser()
+    parser.add_argument('--model', help='Model name to use for classification', type=str)
+    parser.add_argument('--inputDir', help='Where to read the captchas to break. E.g. data/', type=str)
+    parser.add_argument('--output', help='File where the classifications should be saved', type=str)
+    args = parser.parse_args()
+
+    if args.model is None:
+        print("Please specify the model path")
+        exit(1)
+
+    if args.inputDir is None:
+        print("Please specify the directory with captchas to break")
+        exit(1)
+
+    if args.output is None:
+        print("Please specify the path to the output file")
+        exit(1)
+
+
+	interpreter = tflite.Interpreter(model_path=args.model)
 	interpreter.allocate_tensors()
 	input_details = interpreter.get_input_details()
 	output_details = interpreter.get_output_details()
 	input_shape = input_details[0]['shape']
 
-	for i in range(2):
-		file = 'test'+str(i)+'.png';ff = Image.open(file);dd = np.array(ff).astype('float32');
-		dd = np.array([dd/255.0])
-		interpreter.set_tensor(input_details[0]['index'], dd)
-		interpreter.invoke()
-		y_pred = interpreter.get_tensor(output_details[0]['index'])
-		out=my_ctc_decode(y_pred)[:,:6];print(out)
-		out = ''.join([characters[x] for x in out[0]])
-		print(file+" "+out)
+	path=args.inputDir;files=[]
+	for file in os.listdir(path):
+		if file.endswith('.png'):
+			files.append(file)
+	files.sort()
+	with open(args.output, 'w') as output_file:
+		for file in files:
+			f = path+file;ff = Image.open(f);dd = np.array(ff).astype('float32');
+			dd = np.array([dd/255.0])
+			interpreter.set_tensor(input_details[0]['index'], dd)
+			interpreter.invoke()
+			y_pred = interpreter.get_tensor(output_details[0]['index'])
+			out=my_ctc_decode(y_pred)[:,:6];#print(out)
+			out = ''.join([characters[x] for x in out[0]])
+			print(file+" "+out)
+			output_file.write(file + "," + out + "\n")
 
-  
 
 
 
